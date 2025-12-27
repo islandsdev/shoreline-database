6df37061b21566d65ef2db7c3424be11e51cca54
Nadeem <nadeemghaly@gmail.com>
2025-12-27 18:57:59 +0200

    add invoice sync function

---
+++ supabase/functions/Invoice-status-sync/.envs.ts
@@ -0,0 +1,14 @@
+/// <reference path="./deno.d.ts" />
+// Environment Variables Configuration
+export function getEnv() {
+  return {
+    // Supabase Configuration
+    SUPABASE_URL: Deno.env.get("SUPABASE_URL") || "",
+    SUPABASE_SERVICE_ROLE_KEY: Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") || "",
+    // Stripe Configuration
+    STRIPE_API_KEY: Deno.env.get("STRIPE_API_KEY"),
+    STRIPE_API_KEY_TEST: Deno.env.get("STRIPE_API_KEY_TEST"),
+    STRIPE_INVOICE_WEBHOOK_SECRET: Deno.env.get("STRIPE_INVOICE_WEBHOOK_SECRET"),
+    STRIPE_INVOICE_WEBHOOK_SECRET_TEST: Deno.env.get("STRIPE_INVOICE_WEBHOOK_SECRET_TEST")
+  };
+}
