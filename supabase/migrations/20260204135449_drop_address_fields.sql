4ead5537b4c4651737797d7013053dd96d994f00
Nadeem <nadeemghaly@gmail.com>
2026-02-04 15:57:05 +0200

    drop address fields

---
+++ supabase/migrations/20260204135449_drop_address_fields.sql
@@ -0,0 +1,17 @@
+alter table "public"."team_members" drop column "action";
+
+alter table "public"."team_members" drop column "address";
+
+alter table "public"."team_members" drop column "address_1";
+
+alter table "public"."team_members" drop column "address_2";
+
+alter table "public"."team_members" drop column "associated_contracts";
+
+alter table "public"."team_members" drop column "city";
+
+alter table "public"."team_members" drop column "country";
+
+alter table "public"."team_members" drop column "postal_code";
+
+alter table "public"."team_members" drop column "state";
